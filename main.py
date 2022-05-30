e8afe63814842581569c860c48d49abbeaf75c2b
chap985 <fasteroll28@gmail.com>
2022-05-30 17:56:49 +0400

    release v1.1

--- main.py
+++ main.py
@@ -17,21 +17,21 @@ class Converted:
 
 	def convertedJson(self):
 		message = json.dumps(
-			self.msg, sort_keys=True, indent=2,
+			self.msg.to_dict(), sort_keys=True, indent=2,
 			default=str, ensure_ascii=False)
 		
 		with open(f"jsonData/{self.name}_{self.post_id}.json", "a", encoding="utf-8") as file:
 			file.write(f"{message}\n")
 
-	def convertedText(self, message):
+	def convertedText(self):
 		"""
 		В отличии от json содержит в себе только datetime, userId, message.text
 		"""
 		msg = {
-			f"{message.sender.id}": {
-				"username": message.sender.username,
-				"datetime": message.date,
-				"text": message.text
+			f"{self.msg.sender.id}": {
+				"username": self.msg.sender.username,
+				"datetime": self.msg.date,
+				"text": self.msg.text
 			}
 		}
 		message = json.dumps(
@@ -47,8 +47,8 @@ def worker(info) -> None:
 	
 	for message in client.iter_messages(info[-2], reply_to=int(info[-1]), reverse=True):
 		print(f"[*] load {message.sender.id}: {message.text}")
-		cJson = Converted(message.to_dict(), info[-2], info[-1]).convertedJson()
-		cText = Converted(message.to_dict(), info[-2], info[-1]).convertedText(message)
+		cJson = Converted(message, info[-2], info[-1]).convertedJson()
+		cText = Converted(message, info[-2], info[-1]).convertedText()
 	
 	print(f"[+] File {info[-2]}_{info[-1]} successfully saved.")
 
